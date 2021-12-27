fc1d42b575922c3cc48fac81fe1c5e03a5a28194
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2021-12-27 23:45:25 +0530

    Add files via upload

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -1,23 +1,23 @@
 import java.util.LinkedList;
 
-public class WeightedGraph {
-    static class Edge {
+
+class Edges {
         int source;
         int destination;
         int weight;
 
-        public Edge(int source, int destination, int weight) {
+        public Edges(int source, int destination, int weight) {
             this.source = source;
             this.destination = destination;
             this.weight = weight;
         }
     }
 
-    static class Graph {
+public class WeightedGraph {
         int vertices;
-        LinkedList<Edge> [] adjacencylist=null;
+        LinkedList<Edges> [] adjacencylist=null;
 
-        Graph(int vertices) {
+    WeightedGraph(int vertices) {
             this.vertices = vertices;
             adjacencylist = new LinkedList[vertices];
             //initialize adjacency lists for all the vertices
@@ -27,23 +27,23 @@ public class WeightedGraph {
         }
 
         public void addEgde(int source, int destination, int weight) {
-            Edge edge = new Edge(source, destination, weight);
+            Edges edge = new Edges(source, destination, weight);
             adjacencylist[source].addFirst(edge); //for directed graph
         }
 
         public void printGraph(){
             for (int i = 0; i <vertices ; i++) {
-                LinkedList<Edge> list = adjacencylist[i];
+                LinkedList<Edges> list = adjacencylist[i];
                 for (int j = 0; j <list.size() ; j++) {
                     System.out.println("vertex-" + i + " is connected to " +
                             list.get(j).destination + " with weight " + list.get(j).weight);
                 }
             }
         }
-    }
+
     public static void main(String[] args) {
         int vertices = 6;
-        Graph graph = new Graph(vertices);
+        WeightedGraph graph = new WeightedGraph(vertices);
         graph.addEgde(0, 1, 4);
         graph.addEgde(0, 2, 3);
         graph.addEgde(1, 3, 2);
