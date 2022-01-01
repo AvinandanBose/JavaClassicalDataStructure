077842087e11ff835fcc25f240dfa5b6d715ecb0
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-01-02 04:11:29 +0530

    Add files via upload

--- SeiveOfErastosthenes.java
+++ SeiveOfErastosthenes.java
@@ -1,5 +1,4 @@
 package com.company;
-import java.util.Arrays;
 import java.util.Scanner;
 public class SeiveOfErastosthenes {
 
@@ -7,17 +6,15 @@ public class SeiveOfErastosthenes {
         System.out.println("Enter a range:");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         boolean[] isPrime = new boolean[n + 1];
-        isPrime[0] = false;
-        for (int i = 1; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             isPrime[i] = true;
         }
-        for (int i = 1; i <= n; i++) {
-            if (isPrime[i - 1]) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]==true) {
                 System.out.println(i);
-                for (int j = i * i; j <= n; j = j + i) {
-                    isPrime[j - 1] = false;
+                for (int j = i * i; j <= n; j=j +i) {
+                    isPrime[j] = false;
                 }
             }
         }
