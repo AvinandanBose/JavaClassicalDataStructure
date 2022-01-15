0ca7b208a8afa5fc4a9176aa0f0afbc41bbd66d4
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-01-15 21:25:20 +0530

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,8 +1,8 @@
 public class BubbleSort {
     public static void main(String[] args) {
         int arr[] = {12, 3, 2, 20, 1, 8};
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < (arr.length - i) - 1; j++) {
+        for (int i = 0; i < arr.length-1 ; i++) {
+            for (int j = 0; j < (arr.length - i)-1 ; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -11,7 +11,7 @@ public class BubbleSort {
 
             }
         }
-        for (int i = 0; i < arr.length - 1; i++){
+        for (int i = 0; i < arr.length ; i++){
             System.out.println(arr[i]);
         }
 
--- BubbleSort2.java
+++ BubbleSort2.java
@@ -4,7 +4,7 @@ public class BubbleSort2 {
     BubbleSort2(int arr[]) {
         this.arr = arr;
         bubbleSort(arr);
-        for (int i = 0; i < arr.length - 1; i++){
+        for (int i = 0; i < arr.length ; i++){
             System.out.println(arr[i]);
         }
     }
